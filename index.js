54c71191e5c2bb27c0fc96f265315c69b955f5b9
Ian Partridge <i.partridge@uk.ibm.com>
2017-09-03 20:48:36 +0100

    Empty project

---
+++ index.js
@@ -0,0 +1,21 @@
+'use strict';
+
+var Alexa = require('alexa-sdk');
+ 
+exports.handler = function(event, context, callback){
+    var alexa = Alexa.handler(event, context, callback);
+    alexa.registerHandlers(handlers);
+    alexa.execute();
+};
+
+var handlers = { 
+    'LaunchRequest': function () {
+        this.emit('HelloWorldIntent');
+    },
+    'HelloWorldIntent': function () {
+        this.emit(':tell', 'Hello World!');
+    },
+    'TowerIntent': function () {
+	this.emit(':tell', 'Hello Winchester!);
+    },
+};
