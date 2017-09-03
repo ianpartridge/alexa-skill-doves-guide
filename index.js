79ad1951f1c04ba44ece85323648e3be87b6808b
Ian Partridge <i.partridge@uk.ibm.com>
2017-09-03 20:56:15 +0100

    wip

--- index.js
+++ index.js
@@ -18,4 +18,15 @@ var handlers = {
     'TowerIntent': function () {
 	this.emit(':tell', 'Hello Winchester!');
     },
+    'AMAZON.HelpIntent': function () {
+        var speechOutput = "Welcome to Dove's Guide for Church Bell Ringers. What can I help you with?";
+        var reprompt = "What can I help you with?";
+        this.emit(':ask', speechOutput, reprompt);
+    },
+    'AMAZON.CancelIntent': function () {
+        this.emit(':tell', "OK");
+    },
+    'AMAZON.StopIntent': function () {
+        this.emit(':tell', "OK");
+    }
 };
