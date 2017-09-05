395ff05da900f3e84e7cb482cfec852d61c63a30
Ian Partridge <i.partridge@uk.ibm.com>
2017-09-05 21:17:59 +0100

    Include app id

--- index.js
+++ index.js
@@ -1,26 +1,28 @@
 'use strict';
-
 var Alexa = require('alexa-sdk');
- 
-exports.handler = function(event, context, callback){
-    var alexa = Alexa.handler(event, context, callback);
+
+var APP_ID = "amzn1.ask.skill.0b0a3f55-188e-4915-b768-32bc70710451";
+
+exports.handler = function(event, context, callback) {
+    var alexa = Alexa.handler(event, context);
+    alexa.APP_ID = APP_ID;
     alexa.registerHandlers(handlers);
     alexa.execute();
 };
 
-var handlers = { 
+var handlers = {
     'LaunchRequest': function () {
-        this.emit('HelloWorldIntent');
+        this.emit('HelloIntent');
     },
-    'HelloWorldIntent': function () {
-        this.emit(':tell', 'Hello World!');
+    'HelloIntent': function () {
+        this.emit(':tell', "Hello from Dove");
     },
     'TowerIntent': function () {
-	this.emit(':tell', 'Hello Winchester!');
+        this.emit(':tell', "TOWER");
     },
     'AMAZON.HelpIntent': function () {
-        var speechOutput = "Welcome to Dove's Guide for Church Bell Ringers. What can I help you with?";
-        var reprompt = "What can I help you with?";
+        var speechOutput = "Help message";
+        var reprompt = "Help reprompt";
         this.emit(':ask', speechOutput, reprompt);
     },
     'AMAZON.CancelIntent': function () {
